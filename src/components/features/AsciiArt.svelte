780119b9fec3de0f0e9188ec0bc3162923e19e35
tech-MeLD <arieshan136@gmail.com>
2026-06-28 17:46:24 +0800

    change WebGLASCII_color

--- src/components/features/AsciiArt.svelte
+++ src/components/features/AsciiArt.svelte
@@ -6,8 +6,8 @@
    *   import Animation from './ascii/TypeWriter.svelte';   ← current
    *   import Animation from './ascii/WebGLASCII_color.svelte';   ← WebGL ascii color
    */
-  
-  import Animation from '../ascii/TypeWriter.svelte';
+
+  import Animation from '../ascii/WebGLASCII_color.svelte';
 
   interface Props {
     /** 笔记数量 */
