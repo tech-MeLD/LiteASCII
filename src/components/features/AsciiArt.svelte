f0864cfb44c9227e10920dd8bbd48c0b2d7eb73a
tech-MeLD <arieshan136@gmail.com>
2026-06-28 17:48:39 +0800

    change TypeWriter

--- src/components/features/AsciiArt.svelte
+++ src/components/features/AsciiArt.svelte
@@ -7,7 +7,7 @@
    *   import Animation from './ascii/WebGLASCII_color.svelte';   ← WebGL ascii color
    */
 
-  import Animation from '../ascii/WebGLASCII_color.svelte';
+  import Animation from '../ascii/TypeWriter.svelte';
 
   interface Props {
     /** 笔记数量 */
